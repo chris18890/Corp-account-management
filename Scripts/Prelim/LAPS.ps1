eb7feb894113833739868143adc4e68fd4fd6ab0
Chris Murray <chris.f.murray@hotmail.co.uk>
2023-04-30 00:52:53 +0100

    update script for Windows LAPS

--- Scripts/Prelim/LAPS.ps1
+++ Scripts/Prelim/LAPS.ps1
@@ -5,8 +5,13 @@ $Location = "OU=$ParentOU,$EndPath"
 $GPOLocation = "c:\scripts\prelim\gpos"
 $GPOName = "LAPS"
 
-Import-Module AdmPwd.ps
-Update-AdmPwdADSchema
-Set-AdmPwdComputerSelfPermission -OrgUnit "$ParentOU"
+Update-LapsADSchema
+Set-LapsADComputerSelfPermission -Identity "$ParentOU"
+Set-LapsADReadPasswordPermission -Identity "Desktops" -AllowedPrincipals "$Domain\RG_Desktop_Admins"
+Set-LapsADResetPasswordPermission -Identity "Desktops" -AllowedPrincipals "$Domain\RG_Desktop_Admins"
+Set-LapsADReadPasswordPermission -Identity "Laptops" -AllowedPrincipals "$Domain\RG_Desktop_Admins"
+Set-LapsADResetPasswordPermission -Identity "Laptops" -AllowedPrincipals "$Domain\RG_Desktop_Admins"
+Set-LapsADReadPasswordPermission -Identity "Servers" -AllowedPrincipals "$Domain\RG_Server_Admins"
+Set-LapsADResetPasswordPermission -Identity "Servers" -AllowedPrincipals "$Domain\RG_Server_Admins"
 Import-GPO -BackupGpoName $GPOName -TargetName $GPOName -path $GPOLocation -MigrationTable "$GPOLocation\admins.migtable" -CreateIfNeeded
 New-GPLink -name $GPOName -target $Location -LinkEnabled Yes -enforced yes -ErrorAction Stop
