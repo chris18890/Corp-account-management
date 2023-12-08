954ddf36c37af56335f36aa7e7b2ff68d78e8e94
Chris Murray <chris.f.murray@hotmail.co.uk>
2023-12-08 23:16:12 +0000

    add machine2 setup

---
+++ Scripts/Machine2/setup.ps1
@@ -0,0 +1,30 @@
+﻿param([string]$Domain)
+If (!$Domain) {
+    $Domain="$env:userdomain"
+}
+If ($env:computername -ne "$Domain-DC2") {
+    $Machine = "$Domain-DC2"
+    rename-computer $Machine
+}
+Cscript c:\windows\system32\SCRegEdit.wsf /ar 0
+Cscript c:\windows\system32\scregedit.wsf /cs 1
+Cscript c:\windows\system32\scregedit.wsf /AU 4
+$IPNet1 = "10.71.104"
+$IPNetSite = $IPNet1
+$IPAddress = "$IPNetSite.3"
+$Netmask = "21"
+$GatewayAddress = "$IPNet1.1"
+$DNSAddress = @("$IPNet1.2", "$IPNet1.3")
+Get-NetAdapter -Name Ethernet | Rename-NetAdapter -NewName LAN -PassThru
+New-NetIPAddress -IPAddress $IPAddress -InterfaceAlias "LAN" -DefaultGateway $GatewayAddress -AddressFamily IPv4 -PrefixLength $Netmask
+Set-DnsClientServerAddress -InterfaceAlias "LAN" -ServerAddresses $DNSAddress
+Import-Module NetSecurity
+Set-NetFirewallRule -DisplayName "File and Printer Sharing (Echo Request - ICMPv4-In)" -enabled True
+Set-NetFirewallRule -DisplayName "File and Printer Sharing (Echo Request - ICMPv4-Out)" -enabled True
+Set-NetFirewallRule -DisplayName "File and Printer Sharing (SMB-In)" -enabled True
+Set-NetFirewallRule -DisplayName "File and Printer Sharing (SMB-Out)" -enabled True
+Set-NetFirewallRule -DisplayGroup "Windows Remote Management" -enabled True
+Set-NetFirewallRule -DisplayGroup "Remote Volume Management" -enabled True
+Set-NetFirewallRule -DisplayGroup "Remote Event Log Management" -enabled True
+Set-NetFirewallRule -DisplayGroup "Remote Scheduled Tasks Management" -enabled True
+Restart-Computer
