e308ce9dcc6de37035a4e959418222e93b574ecf
Chris Murray <chris.f.murray@hotmail.co.uk>
2024-03-23 00:26:12 +0000

    add machine3 setup

---
+++ Scripts/Machine3/dcpromo.ps1
@@ -0,0 +1,10 @@
+﻿param([string]$Domain)
+If (!$Domain) {
+    $Domain = $env:userdomain
+}
+$ComputerName = $env:computername
+if ((gwmi win32_computersystem).partofdomain -eq $false) {
+    Add-Computer -DomainName "$Domain" -Restart
+}
+Install-WindowsFeature -name AD-Domain-Services, FS-DFS-Namespace, FS-DFS-Replication -IncludeManagementTools -ComputerName $ComputerName
+Install-ADDSDomainController -Credential (Get-Credential) -DomainName "$Domain" -InstallDns:$true -NoGlobalCatalog:$false -Force:$true
