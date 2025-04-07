7389c0a48388c9d1f2bb3403f5f226e24542f32d
Chris Murray <chris.f.murray@hotmail.co.uk>
2025-04-07 12:32:40 +0100

    add machine3 setup

---
+++ Scripts/Machine3/dcpromo.ps1
@@ -0,0 +1,14 @@
+﻿param([string]$Domain,[string]$DomainSuffix)
+If (!$Domain) {
+    $Domain = $env:userdomain
+}
+$ComputerName = $env:computername
+if ((gwmi win32_computersystem).partofdomain -eq $false) {
+    If (!$DomainSuffix) {
+        $DomainSuffix = READ-HOST 'Enter a public FQDN- '
+    }
+    Add-Computer -DomainName "$Domain.$DomainSuffix" -Restart
+} else {
+    Install-WindowsFeature -name AD-Domain-Services, FS-DFS-Namespace, FS-DFS-Replication -IncludeManagementTools -ComputerName $ComputerName
+    Install-ADDSDomainController -Credential (Get-Credential) -DomainName "$Domain" -InstallDns:$true -NoGlobalCatalog:$false -Force:$true
+}
