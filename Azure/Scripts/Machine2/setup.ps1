eb4b35e185feecb9c898f781bb7394692c822be0
Chris Murray <chris.f.murray@hotmail.co.uk>
2025-04-07 12:32:12 +0100

    add machine2 setup

---
+++ Azure/Scripts/Machine2/setup.ps1
@@ -0,0 +1,20 @@
+﻿param([string]$Domain)
+If (!$Domain) {
+    $Domain="$env:userdomain"
+}
+If ($env:computername -ne "$Domain-DC2") {
+    $Machine = "$Domain-DC2"
+    rename-computer $Machine
+}
+Cscript c:\windows\system32\SCRegEdit.wsf /ar 0
+Cscript c:\windows\system32\scregedit.wsf /cs 1
+Cscript c:\windows\system32\scregedit.wsf /AU 4
+Import-Module NetSecurity
+Set-NetFirewallRule -DisplayName "File and Printer Sharing (Echo Request - ICMPv4-In)" -enabled True
+Set-NetFirewallRule -DisplayName "File and Printer Sharing (Echo Request - ICMPv4-Out)" -enabled True
+Set-NetFirewallRule -DisplayName "File and Printer Sharing (SMB-In)" -enabled True
+Set-NetFirewallRule -DisplayName "File and Printer Sharing (SMB-Out)" -enabled True
+Set-NetFirewallRule -DisplayGroup "Windows Remote Management" -enabled True
+Set-NetFirewallRule -DisplayGroup "Remote Volume Management" -enabled True
+Set-NetFirewallRule -DisplayGroup "Remote Event Log Management" -enabled True
+Set-NetFirewallRule -DisplayGroup "Remote Scheduled Tasks Management" -enabled True
